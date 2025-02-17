ae1e08a62f6b7241cf9be2760bb8a1e4aaf375d0
ordo-chao <princeben9312@gmail.com>
2025-02-17 10:36:03 +0300

    Final pages added

--- src/pages/hackathon.jsx
+++ src/pages/hackathon.jsx
@@ -1,12 +1,23 @@
 import Layout from "/src/layout/homepage_layout.jsx";
 import "/src/Css/hackathon.css";
+import PropagateLoader from "react-spinners/PropagateLoader";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 function Hackathon() {
   return (
     <Layout>
       <div>
-        <h1>Hackathon</h1>
-        <p>Our company is the best in the world.</p>
+        <h1 className="Event__page_title">Hackathon</h1>
+        <hr />
+         <div className="hackathon" >
+         <DotLottieReact
+                          src="https://lottie.host/08238f7a-4b5d-4237-ac9d-bb298c0f2db0/mIwHBP3n1v.lottie"
+                          loop
+                          autoplay
+                          style={{ width: '500px', height: '500px' }}
+                        />
+                        <p>No hackathon to display</p>
+         </div>
       </div>
     </Layout>
   );
